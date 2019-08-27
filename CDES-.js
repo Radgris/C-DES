9ed4136d28b83e8e120d4f1fc00110a6f4e03619
Radgris <golden_run@hotmail.com>
2019-08-27 11:33:15 -0600

    first manual
    Added comments on necessary things

---
+++ CDES-.js
@@ -0,0 +1,8 @@
+/*
+
+use bitwise operation to manipulate bits, these operation can be used with decimal numbers and will directly convert internally to bit operations
+note: the operation that requires us to "cycle" bits is still pending, probably a custom function: https://stackoverflow.com/a/1768055
+we also need a function that takes 2 array of bits ( not necesarrily an array structure) and shifts array A based on array's B content: (bit access)https://stackoverflow.com/a/9954810
+https://www.w3schools.com/js/js_bitwise.asp
+
+*/  
